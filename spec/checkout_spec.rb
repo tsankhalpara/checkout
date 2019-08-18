a16b24f4c5cf12e4b6d33bd355b23395b4534b91
Tara Sankhalpara <tsankhalpara@mac.com>
2019-08-18 20:08:01 +0100

    add test to add item to basket

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Checkout do
+
+  describe "scan" do
+    it "adds item to basket from product code" do
+      co = Checkout.new
+      co.scan(001)
+      expect(co.basket).to eq [1]
+    end
+  end
+
+end
