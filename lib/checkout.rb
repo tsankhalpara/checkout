d542b87f5e1b074b221c26dd5ddb6c49f8cf45af
Tara Sankhalpara <tsankhalpara@mac.com>
2019-08-19 14:10:48 +0100

    refactor checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -7,12 +7,10 @@ class Checkout
     @items = {001 => 9.25, 002 => 45.00, 003 => 19.95}
     @basket = []
     @promotional_rules = promotional_rules
-    @price = 0.00
   end
 
   def scan(item)
     @basket << item
-    @price += @items[item]
   end
 
   def total
