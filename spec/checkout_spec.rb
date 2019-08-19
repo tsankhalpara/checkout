cd51f8e0abe11a95f1141607ea9d8e0177136310
Tara Sankhalpara <tsankhalpara@mac.com>
2019-08-19 11:21:28 +0100

    add test for 10% off purchases overt for 10% off purchases over £60

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -17,6 +17,13 @@ describe Checkout do
       co.scan(001)
       expect(co.total).to eq 9.25
     end
+
+    it "takes 10% off purchases over £60" do
+      co = Checkout.new
+      co.scan(002)
+      co.scan(002)
+      expect(co.total).to eq 81.00
+    end
   end
 
 end
